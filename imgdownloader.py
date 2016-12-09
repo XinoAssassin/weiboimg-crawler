a4cacf8ea6b6859791976cdafc5f1af1ef0b1053
TimeCompass <hourglassmiku@gmail.com>
2016-12-09 17:16:22 +0800

    fix api

--- imgdownloader.py
+++ imgdownloader.py
@@ -1,44 +1,51 @@
+'''微博配图爬虫'''
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import requests
-import sqlite3
 import multiprocessing
 import os
+import sqlite3
+import requests
 
-def Downloader(pic_id=None,gif=None):
-    if gif == 1:
-        url = "http://ww4.sinaimg.cn/large/{pic_id}.gif".format(pic_id=pic_id)
-        filename = pic_id + '.gif'
+multiprocessing.freeze_support()
+
+
+def downloader(picid, belong):
+    '''专心下载'''
+    workpath = os.path.abspath('.')
+    if not picid:
+        return 404
     else:
-        url = "http://ww4.sinaimg.cn/large/{pic_id}.jpg".format(pic_id=pic_id)
-        filename = pic_id + '.jpg'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'}
-    file = requests.get(url=url, headers=headers)
-    print(u'正在下载 {filename}'.format(filename=filename))
-    if os.path.exists(filename):
-        print(u'{filename} 已存在，跳过下载过程'.format(filename=filename))
+        filename = picid.split('/')[-1]
+    if os.path.exists(belong):
+        pass
     else:
-        with open(filename, 'wb') as f:
-            f.write(file.content)
-        f.close()
+        os.mkdir(belong)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'}
+    file = requests.get(url=picid, headers=headers)
+    filesize = file.headers.get('Content-Length')
+    os.chdir(belong)
+    if os.path.exists(filename) and (os.path.getsize(filename) == int(filesize)):
+        print(u'{0} 已存在，跳过下载过程'.format(filename))
+    else:
+        filepath = workpath + '\\' + belong + '\\' + filename
+        with open(filepath, 'wb') as imgfile:
+            print(u'正在下载 {0}'.format(filename))
+            imgfile.write(file.content)
+        imgfile.close()
+    os.chdir(workpath)
 
 
-def WriteData(nickname=None, created_at=None, text=None, pic_ids=None, gif_ids=None):
-    pic_db = sqlite3.connect('{nickname}.db'.format(nickname=nickname))
-    curs = pic_db.cursor()
-    curs.execute('CREATE TABLE IF NOT EXISTS weibolist (Id CHAR(40) PRIMARY KEY, WeiboText CHAR(200), Created_at CHAR(20))')
-    ins = 'INSERT OR REPLACE INTO weibolist VALUES(?, ?, ?)'
-    for pic_id in pic_ids:
-        curs.execute(ins, (pic_id, text, created_at))
-    pic_db.commit()
-    curs.close()
-    pic_db.close()
-    for pic_id in pic_ids:
-        if not gif_ids:
-            t = multiprocessing.Process(target=Downloader, args=(pic_id, 0))
-        elif pic_id in gif_ids:
-            t = multiprocessing.Process(target=Downloader, args=(pic_id, 1))
-        else:
-            t = multiprocessing.Process(target=Downloader, args=(pic_id, 0))
-        t.start()
+def writedata(nick, link, text, pics):
+    '''写数据库'''
+    weibodata = sqlite3.connect('weibo.db')
+    localdb = weibodata.cursor()
+    localdb.execute('CREATE TABLE IF NOT EXISTS weibolist \
+    (Nick CHAR(40), WeiboLink CHAR(40), WeiboText CHAR(1000), ImageLink CHAR(80) PRIMARY KEY)')
+    ins = 'INSERT OR REPLACE INTO weibolist VALUES(?, ?, ?, ?)'
+    for pic in pics:
+        worker = multiprocessing.Process(target=downloader, args=(pic, nick))
+        localdb.execute(ins, (nick, link, text, pic))
+        weibodata.commit()
+        worker.start()
--- main.py
+++ main.py
@@ -1,101 +1,62 @@
+'''微博配图爬虫'''
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import requests
 import re
-import json
-import os
-from bs4 import BeautifulSoup
-from imgdownloader import WriteData
+import requests
+from imgdownloader import writedata
 
+API = 'http://m.weibo.cn/container/getIndex?'
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) \
+           AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'}
 
-def GetID(nickname=None):
-    if nickname:
-        url = 'http://m.weibo.cn/n/{nickname}'.format(nickname=nickname)
-    else:
-        return 404
-    try:
-        tmp = requests.get(url=url)
-        m_page = BeautifulSoup(tmp.text, "lxml")
-        fid = re.findall(fid_re, m_page.body.script.text)[0]
-        return int(fid)
-    except ValueError:
-        return 404
-
-
-def PageCount(url=None):
-    # Return Page Number
-    tmp = requests.get(url=url)
-    try:
-        content = json.loads(tmp.text)
-        if content['ok'] == 1:
-            total_w = content['count']
-            page = total_w // 10 if total_w % 10 == 0 else total_w // 10 + 1
-            return page
-        else:
-            # Unknow Error?
-            pass
-    except ValueError:
-        return None
-
-
-def CardInfo(nickname=None, card=None):
-    # Original Weibo Only
+def cardinfo(nick, card):
+    '''写数据库并下载配图'''
+    if card['card_type'] == 11:
+        return 302
     if card['mblog'].get('retweeted_status'):
+        # 跳过转发内容
         pass
-    elif card['mblog']['pic_ids']:
-        created_at = card['mblog']['created_at']
-        # Need Text Only
-        text = re.sub('<.+?>', '', card['mblog']['text'])
-        pic_ids = card['mblog']['pic_ids']
-        gifs = card['mblog']['gif_ids'].replace(',', '|')
-        gif_ids = [x for x in gifs.split('|')]
-        WriteData(nickname, created_at, text, pic_ids, gif_ids)
+    elif card['mblog'].get('pics'):
+        weibolink = card['scheme']
+        # 过滤微博内容中的超链接
+        weibotext = re.sub('<.+?>', '', card['mblog']['text'])
+        weibopics = card['mblog'].get('pics')
+        # weibogifs = card['mblog']['gif_ids'].replace(',', '|')
+        # weibogifs = [x for x in weibogifs.split('|')]
+        weibopics = [x.get('large')['url'] for x in weibopics]
+        writedata(nick, weibolink, weibotext, weibopics)
     else:
         pass
 
 
-def GetJson(nickname=None, fid=None, page=None):
-    if fid:
-        url = 'http://m.weibo.cn/page/json?containerid={fid}_-_WEIBO_SECOND_PROFILE_WEIBO'.format(fid=fid)
-    else:
-        return 404
-    if page:
-        url += '&page={page}'.format(page=page)
-    else:
-        # If page, Get data, If not, Get page
-        return PageCount(url=url)
-    tmp = requests.get(url=url)
-    try:
-        content = json.loads(tmp.text)
-        if content['ok'] == 1:
-            if content['cards'][0]['mod_type'] == "mod/empty":
-                return 200
-            else:
-                card_group = content['cards'][0]['card_group']
-                for card in card_group:
-                    CardInfo(nickname, card)
+def getcontent(nick):
+    '''API接口获取微博内容'''
+    indexurl = 'http://m.weibo.cn/n/{nick}'.format(nick=nick)
+    weibocokie = requests.get(url=indexurl).headers.get('Set-Cookie')
+    weibofid = re.findall(r'\d{16}', weibocokie)[0]
+    usercont = requests.get(url=API + 'containerid={0}'.format(weibofid)).json()
+    usertabs = usercont['tabsInfo']['tabs']
+    for tab in usertabs:
+        if tab.get('title') == "微博":
+            containerid = tab.get('containerid')
+    page = 1
+    params = {'containerid': containerid}
+    weibocont = requests.get(url=API, headers=HEADERS, params=params).json()
+    print(containerid)
+    while weibocont.get('ok') == 1:
+        if weibocont.get('cards'):
+            contentcards = weibocont.get('cards')
+            for card in contentcards:
+                cardinfo(nick, card)
         else:
-            # Unknow Error
-            pass
-    except ValueError:
-        return None
+            break
+        page += 1
+        params = {'containerid': containerid, 'page': page}
+        weibocont = requests.get(url=API, headers=HEADERS, params=params).json()
 
 
 if __name__ == '__main__':
-    fid_re = re.compile(r"stageId\':\'(\d+)")
-    for nickname in open('nickname.txt'):
-        nickname = nickname.strip('\n').strip('\r')
-        if os.path.exists(nickname):
-            os.chdir(nickname)
-        else:
-            os.mkdir(nickname)
-            os.chdir(nickname)
-        page = GetJson(GetID(nickname))
-        fid = GetID(nickname)
-        if page:
-            for x in range(1, page + 1):
-                GetJson(nickname, fid, x)
-            os.chdir('..')
-        else:
-            pass
+    for name in open('nickname.txt'):
+        name = name.strip('\n').strip('\r')
+        getcontent(name)
